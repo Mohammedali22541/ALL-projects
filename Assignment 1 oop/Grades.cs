1237598485c36f20b9e5836f9bb6bae84eb1e1d9
Mohammedali22541 <Mohammedali792406@gmail.com>
2025-07-27 09:20:42 +0300

    oop 1

---
+++ Assignment 1 oop/Grades.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_oop
+{
+    internal enum Grades
+    {
+        a = 1, b, c, d, e, f,
+
+        exellant = 1
+    }
+}
---
+++ Assignment 1 oop/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_oop
+{
+    [Flags]
+    internal enum Permissions : byte
+    {   
+        read = 1,
+        write = 2,
+        delete = 4,
+        execute = 8,
+    }
+}
