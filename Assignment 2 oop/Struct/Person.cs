8b87c1567616e42d9a6db2e99aefd7cee844f81d
Mohammedali22541 <Mohammedali792406@gmail.com>
2025-07-31 05:43:07 +0300

    Assignment 2 oop

---
+++ Assignment 2 oop/Struct/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_oop.Struct
+{
+   
+    internal struct Person
+    {
+        #region Attributes
+        public string? name;
+        public int age;
+        #endregion
+
+
+        #region Method
+        public override string ToString()
+        {
+            return $"name: {name} , age: {age}";
+        } 
+        #endregion
+    }
+
+}
---
+++ Assignment 2 oop/Struct/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_oop.Struct
+{
+    internal struct Point
+    {
+        #region Attributes
+        public int x;
+        public int y;
+        #endregion
+
+        public Point(int X , int Y )
+        {
+            x = X;
+            y = Y;
+
+
+        }
+
+        
+
+        public override string ToString() {
+            return $"x = {x} , y = {y}";
+        }
+    }
+    
+     
+
+}
